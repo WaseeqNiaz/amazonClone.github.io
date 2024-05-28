29ecc97b58cd2f1c8981f26e1eec938898c22ce3
WaseeqNiaz <waseeqniaz@gmail.com>
2024-05-28 15:14:44 +0500

    Add files via upload

---
+++ Amazon/script.js
@@ -0,0 +1,8 @@
+// // let a=document.querySelectorAll('#nav-search');
+// function Changecolor() {
+//      let a=document.getElementById('nav-search')
+//     a.style.border = "2px solid rgb(8, 0, 255)";
+//     a.style.boxShadow = " rgba(72, 81, 202, 0.8) 0 0 30px 7px";
+//     a.style.borderRadius = "7px";
+// }
+confirm("My First Colne Website")
